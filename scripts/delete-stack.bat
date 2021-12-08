2945b5b63c50f65fa6fb88fbe0a1611bddd85f07
Muhtashim Iqbal <muhtashim.iqbal@Cdocs.local>
2021-12-08 16:51:10 +0500

    corrected batch scripts

--- scripts/delete-stack.bat
+++ scripts/delete-stack.bat
@@ -1,10 +1,9 @@
-$STACK_NAME=$env:STACK_NAME
-$REGION="us-east-1"
+set REGION="us-east-1"
 
 aws cloudformation delete-stack \
---stack-name $STACK_NAME \
---region $REGION
+--stack-name %STACK_NAME% \
+--region %REGION%
 
 aws cloudformation wait stack-delete-complete \
---stack-name $STACK_NAME \
---region $REGION
+--stack-name %STACK_NAM% \
+--region %REGION%
--- scripts/deploy-stack.bat
+++ scripts/deploy-stack.bat
@@ -1,8 +1,4 @@
-$STACK_NAME=$env:STACK_NAME
-$PARAMETERS_FILE_NAME=$env:PARAMETERS_FILE_NAME
-$TEMPLATE_NAME=$env:TEMPLATE_NAME
-$CHANGESET_MODE=$env:CHANGESET_MODE
-$REGION="us-east-1"
+set REGION="us-east-1"
 
 if "cloudformation/"$TEMPLATE_NAME NEQ *.yaml( 
     echo "CloudFormation template $TEMPLATE_NAME does not exist. Make sure the extension is *.yaml and not (*.yml)"
@@ -14,16 +10,14 @@ if "parameters/"$PARAMETERS_FILE_NAME NEQ *.properties()
 
 if  $CHANGESET_MODE == "true"  OR $CHANGESET_MODE == "True" (
     aws cloudformation deploy \
-    --stack-name $STACK_NAME \
-    --template-file cloudformation/$TEMPLATE_NAME \
-    --parameter-overrides file://parameters/$PARAMETERS_FILE_NAME \
-    --capabilities CAPABILITY_NAMED_IAM \
-    --region $REGION)
+    --stack-name %STACK_NAME% \
+    --template-file cloudformation/%TEMPLATE_NAME% \
+    --parameter-overrides file://parameters/%PARAMETERS_FILE_NAME% \
+    --region %REGION%)
 else
     (aws cloudformation deploy \
-    --stack-name $STACK_NAME \
-    --template-file cloudformation/$TEMPLATE_NAME \
-    --parameter-overrides file://parameters/$PARAMETERS_FILE_NAME \
-    --capabilities CAPABILITY_NAMED_IAM \
-    --region $REGION \
+    --stack-name %STACK_NAME% \
+    --template-file cloudformation/%TEMPLATE_NAME% \
+    --parameter-overrides file://parameters/%PARAMETERS_FILE_NAME% \
+    --region %REGION% \
     --no-execute-changeset)
