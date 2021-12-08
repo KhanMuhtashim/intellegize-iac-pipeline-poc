669f7459b08c38b699146688cbc9674a2970314d
Muhtashim Iqbal <muhtashim.iqbal@Cdocs.local>
2021-12-08 17:22:27 +0500

    corrected batch scripts

--- scripts/deploy-stack.bat
+++ scripts/deploy-stack.bat
@@ -2,13 +2,6 @@ echo %TEMPLATE_NAME%
 echo  %STACK_NAME%
 echo %CHANGESET_MODE%
 echo %PARAMETERS_FILE_NAME%
-if /i "cloudformation/"%TEMPLATE_NAME%%~x1 NEQ ".yaml" (
-    echo "CloudFormation template $TEMPLATE_NAME does not exist. Make sure the extension is *.yaml and not (*.yml)"
-    exit 0)
-
-if /i "parameters/"%PARAMETERS_FILE_NAME%%~x1 NEQ ".properties"(
-    echo "CloudFormation parameters $PARAMETERS_FILE_NAME does not exist"
-    exit 0)
 
 if  %CHANGESET_MODE% == "true"  OR %CHANGESET_MODE% == "True" (
     aws cloudformation deploy \
