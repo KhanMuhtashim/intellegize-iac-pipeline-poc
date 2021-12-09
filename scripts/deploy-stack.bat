e704d1450d12badd96bc6524b2c2a9ee655ae37e
Muhtashim Iqbal <muhtashim.iqbal@Cdocs.local>
2021-12-09 12:40:57 +0500

    fixed JEnkinsfile

--- scripts/deploy-stack.bat
+++ scripts/deploy-stack.bat
@@ -3,9 +3,4 @@ echo %STACK_OR_CHANGESET_NAME%
 echo %CHANGESET_MODE%
 echo %PARAMETERS_FILE_NAME%
 
-if  %CHANGESET_MODE%==true (
-    aws cloudformation create-change-set --stack-name %STACK_OR_CHANGESET_NAME% --change-set-name %STACK_OR_CHANGESET_NAME% --template-body cloudformation/%TEMPLATE_NAME%
-    aws cloudformation describe-change-set --stack-name %STACK_OR_CHANGESET_NAME% --change-set-name %STACK_OR_CHANGESET_NAME%
-) else  (
-    aws cloudformation deploy --stack-name %STACK_OR_CHANGESET_NAME% --template-file cloudformation/%TEMPLATE_NAME% --parameter-overrides file://parameters/%PARAMETERS_FILE_NAME% --no-execute-changeset
-)
+if  %CHANGESET_MODE%==true ( echo hello) else  (echo nothello)
