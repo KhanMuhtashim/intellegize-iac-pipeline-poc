cdf01a5db61c15ecea46d08fbdd8d0744a465601
Muhtashim Iqbal <muhtashim.iqbal@Cdocs.local>
2021-12-09 15:13:12 +0500

    fixed path

--- scripts/create-change-set.bat
+++ scripts/create-change-set.bat
@@ -2,5 +2,5 @@ echo %TEMPLATE_NAME%
 echo %STACK_OR_CHANGESET_NAME%
 
 
-aws cloudformation create-change-set --stack-name %STACK_OR_CHANGESET_NAME% --change-set-name %STACK_OR_CHANGESET_NAME% --template-body cloudformation/%TEMPLATE_NAME% --parameters parameters/%PARAMETERS_FILE_NAME%
+aws cloudformation create-change-set --stack-name %STACK_OR_CHANGESET_NAME% --change-set-name %STACK_OR_CHANGESET_NAME% --template-body cloudformation/%TEMPLATE_NAME% --parameters file://parameters/%PARAMETERS_FILE_NAME%
 aws cloudformation describe-change-set --stack-name %STACK_OR_CHANGESET_NAME% --change-set-name %STACK_OR_CHANGESET_NAME%
