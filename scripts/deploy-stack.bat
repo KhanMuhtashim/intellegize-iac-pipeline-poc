9566382ae3b800d74efdc95fe4d00a4e779d596f
Muhtashim Iqbal <muhtashim.iqbal@Cdocs.local>
2021-12-09 15:21:13 +0500

    fixed create stack file

--- scripts/deploy-stack.bat
+++
@@ -1,5 +0,0 @@
-echo %TEMPLATE_NAME%
-echo %STACK_OR_CHANGESET_NAME%
-echo %PARAMETERS_FILE_NAME%
-
-aws cloudformation deploy --stack-name %STACK_OR_CHANGESET_NAME% --template-file cloudformation/%TEMPLATE_NAME% --parameter-overrides file://parameters/%PARAMETERS_FILE_NAME% --no-execute-changeset
