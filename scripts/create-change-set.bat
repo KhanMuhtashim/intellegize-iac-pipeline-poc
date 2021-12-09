a4ff4cdd590fe95f59fde6d971d14c98dbece49b
Muhtashim Iqbal <muhtashim.iqbal@Cdocs.local>
2021-12-09 17:52:57 +0500

    changed change set bat

--- scripts/create-change-set.bat
+++ scripts/create-change-set.bat
@@ -4,4 +4,6 @@ echo %STACK_OR_CHANGESET_NAME%
 
 aws cloudformation create-change-set --stack-name %STACK_OR_CHANGESET_NAME% --change-set-name %STACK_OR_CHANGESET_NAME% --template-body file://cloudformation/%TEMPLATE_NAME% --parameters file://parameters/%PARAMETERS_FILE_NAME%
 echo ==============================================================================================================================================================================================================================
+aws cloudformation wait change-set-create-complete --stack-name %STACK_OR_CHANGESET_NAME% --change-set-name %STACK_OR_CHANGESET_NAME%
+echo ==============================================================================================================================================================================================================================
 aws cloudformation describe-change-set --stack-name %STACK_OR_CHANGESET_NAME% --change-set-name %STACK_OR_CHANGESET_NAME%
