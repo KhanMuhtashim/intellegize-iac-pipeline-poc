83519ab5865dde95819f9f58d5ba7ddc5df1ce1a
Muhtashim Iqbal <muhtashim.iqbal@Cdocs.local>
2021-12-09 19:41:05 +0500

    changed bucket name

--- scripts/create-stack.bat
+++ scripts/create-stack.bat
@@ -4,4 +4,6 @@ echo %PARAMETERS_FILE_NAME%
 
 aws cloudformation create-stack --stack-name %STACK_OR_CHANGESET_NAME% --template-body file://cloudformation/%TEMPLATE_NAME% --parameters file://parameters/%PARAMETERS_FILE_NAME%
 echo ===============================================================================================================================================================================
+aws cloudformation wait stack-create-complete --stack-name %STACK_OR_CHANGESET_NAME%
+echo ===============================================================================================================================================================================
 aws cloudformation describe-stacks --stack-name %STACK_OR_CHANGESET_NAME%
