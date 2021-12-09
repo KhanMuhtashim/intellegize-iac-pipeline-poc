3ae2ae578da117d77d9de30b3868c74931a76bb3
Muhtashim Iqbal <muhtashim.iqbal@Cdocs.local>
2021-12-09 15:24:38 +0500

    fixed create stack file

--- scripts/create-stack.bat
+++ scripts/create-stack.bat
@@ -2,6 +2,6 @@ echo %TEMPLATE_NAME%
 echo %STACK_OR_CHANGESET_NAME%
 echo %PARAMETERS_FILE_NAME%
 
-aws cloudformation create-stack --stack-name %STACK_OR_CHANGESET_NAME% --template-body file://cloudformation/%TEMPLATE_NAME% --parameters file://parameters/%PARAMETERS_FILE_NAME% --no-execute-changeset
+aws cloudformation create-stack --stack-name %STACK_OR_CHANGESET_NAME% --template-body file://cloudformation/%TEMPLATE_NAME% --parameters file://parameters/%PARAMETERS_FILE_NAME%
 echo ================================================================================================================================
 aws cloudformation describe-stacks --stack-name %STACK_OR_CHANGESET_NAME%
