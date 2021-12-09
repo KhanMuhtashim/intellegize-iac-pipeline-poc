9d21ec17e945f41b7acd1af9f882883b9f418e7f
Muhtashim Iqbal <muhtashim.iqbal@Cdocs.local>
2021-12-09 11:54:56 +0500

    added describe change set command

--- scripts/deploy-stack.bat
+++ scripts/deploy-stack.bat
@@ -5,6 +5,7 @@ echo %PARAMETERS_FILE_NAME%
 
 if  %CHANGESET_MODE% == "true"  OR %CHANGESET_MODE% == "True" (
     aws cloudformation create-change-set --stack-name %STACK_OR_CHANGESET_NAME% --change-set-name %STACK_OR_CHANGESET_NAME% --template-body cloudformation/%TEMPLATE_NAME%
+    aws cloudformation describe-change-set --change-set-name %STACK_OR_CHANGESET_NAME%
 ) else  (
     aws cloudformation deploy --stack-name %STACK_OR_CHANGESET_NAME% --template-file cloudformation/%TEMPLATE_NAME% --parameter-overrides file://parameters/%PARAMETERS_FILE_NAME% --no-execute-changeset
 )
